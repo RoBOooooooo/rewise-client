80699a28337f0d31e4eeed76a1712452dafc7b3e
Mujahidul Islam Arif <mujahidarif120@gmail.com>
2025-12-19 12:17:30 +0600

    setup project folder structure and route constants

---
+++ src/routes/paths.js
@@ -0,0 +1,20 @@
+export const PATHS = {
+    HOME: '/',
+    LOGIN: '/login',
+    REGISTER: '/register',
+    ALL_LESSONS: '/lessons',
+    LESSON_DETAILS: '/lessons/:id',
+    DASHBOARD: {
+        ROOT: '/dashboard',
+        ADD_LESSON: 'add-lesson',
+        MY_LESSONS: 'my-lessons',
+        PROFILE: 'profile',
+        UPDATE_LESSON: 'update-lesson/:id',
+    },
+    ADMIN: {
+        ROOT: '/admin',
+        DASHBOARD: 'dashboard',
+        USERS: 'users',
+        LESSONS: 'lessons',
+    },
+};
